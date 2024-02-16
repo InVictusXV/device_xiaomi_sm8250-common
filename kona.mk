8b4937b246695d7b8985ea09d61fa56d2114b91b
zclkkk <zclkkk@outlook.com>
2024-02-16 20:07:37 +0000

    sm8250-common: Bring back QTI perfd
    Change-Id: I61d6ba80c3bd8f6383f4e6c9a96f7616ab19c88d

--- kona.mk
+++ kona.mk
@@ -301,6 +301,14 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Car
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
