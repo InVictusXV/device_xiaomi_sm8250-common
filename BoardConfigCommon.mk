7e7ee0b1eb28c3610796c3a5a50de59615c6275e
darkphnx <dark.phnx12@gmail.com>
2024-11-26 20:36:22 +0000

    sm8250-common:Set VENDOR_SECURITY_PATCH same as PLATFORM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -200,7 +200,7 @@ SOONG_CONFIG_XIAOMI_KONA_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-06-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
