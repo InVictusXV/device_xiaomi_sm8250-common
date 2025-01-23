3ae34648fcdfdb3ed4079c5f198b1529217105b6
Michael Bestas <mkbestas@lineageos.org>
2025-01-23 07:26:29 +0100

    sm8250-common: Migrate mount point creation out of Android.mk Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- kona.mk
+++ kona.mk
@@ -312,6 +312,12 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
+# Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
